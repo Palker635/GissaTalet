388f37cb615454505be83bf695abd6225c848622
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2019-10-17 11:21:44 +0200

    Torsdag

---
+++ GissaTalet/src/GissaTalet.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import javax.swing.*;
+public class GissaTalet {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        double n = rand.nextInt(100);
+        double gissa = Double.parseDouble (.showInputDialog(null, "Gissa Talet!"));
+             if (gissa = n) {
+            JOptionPane.showMessageDialog(null, "Du gissade rätt!");
+        }  else {
+
+                }
+            }
+        }
+    }
+}
