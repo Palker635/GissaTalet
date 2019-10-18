ae9a63c9e896f0422a0bb70d6a9147a1e39c19b7
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2019-10-18 10:50:40 +0200

    Fredag - Klart!

--- GissaTalet/src/GissaTalet.java
+++ GissaTalet/src/GissaTalet.java
@@ -1,17 +1,57 @@
 import java.util.Random;
 import javax.swing.*;
+
 public class GissaTalet {
 
     public static void main(String[] args) {
-        Random rand = new Random();
-        double n = rand.nextInt(100);
-        double gissa = Double.parseDouble (.showInputDialog(null, "Gissa Talet!"));
-             if (gissa = n) {
-            JOptionPane.showMessageDialog(null, "Du gissade rätt!");
-        }  else {
 
-                }
+        int antal = 0;
+        Random rand = new Random();
+        int n = rand.nextInt(100);
+        //Generar ett nytt tal för "n" när n = 0 eftersom uppgiften säger att talet ska vara mellan 1 och 100.
+        while (n == 0) {
+            n = rand.nextInt(100);
+        }
+        int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Gissa Talet!"));
+        if (a == n) {
+            JOptionPane.showMessageDialog(null, "Du gissade rätt!");
+            JOptionPane.showMessageDialog(null, "Du vann efter " + antal + " försök!");
+        }
+        while (a != n) {
+            if (a < n) {
+                antal++;
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "För lågt! Försök igen!"));
+            } else {
+                antal++;
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "För högt! Försök igen!"));
+            } if (a == n){
+
+                JOptionPane.showMessageDialog(null, "Du gissade rätt!");
+                JOptionPane.showMessageDialog(null, "Du vann efter " + antal + " försök!");
             }
         }
     }
 }
+// Skräp nedan
+/*
+        // antal = antalet försök som spelaren har gjort
+        int antal = 0;
+        Random rand = new Random();
+        // n = datorns slumpade tal
+        double n = rand.nextInt(100);
+        // a = Spelarens gissning
+        double a = Double.parseDouble(JOptionPane.showInputDialog(null, "Gissa Talet!"));
+        if (a == n) {
+            JOptionPane.showMessageDialog(null, "Du gissade rätt!");
+            JOptionPane.showMessageDialog("Du vann efter " + antal + "försök!");
+        } else if (a > n) {
+            antal++;
+            double a = Double.parseDouble(JOptionPane.showInputDialog(null, "För högt! Försök igen!"));
+        } else if (a < n) {
+            antal++;
+            double a = Double.parseDouble(JOptionPane.showInputDialog(null, "För lågt! Försök igen!"));
+        }
+    }
+}
+
+*/
